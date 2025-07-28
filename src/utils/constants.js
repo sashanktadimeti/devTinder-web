d36caa8c121502c131982d253da367209dbcb860
sashank tadimeti <sashanktadimeti4@gmail.com>
2025-07-28 23:01:42 +0530

    feat: added hide and show password , url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "/api/"
 export const EYE_OPEN = "https://img.icons8.com/ios-filled/50/000000/visible.png";
 export const EYE_CLOSED = "https://static.vecteezy.com/system/resources/thumbnails/013/729/597/small_2x/show-password-icon-eye-symbol-vision-hide-from-watch-icon-secret-view-web-design-element-vector.jpg";
